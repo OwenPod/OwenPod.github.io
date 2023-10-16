81a562977b8d630d258d664ae1bc90ed16c94d97
Owen <podziemskio@msoe.edu>
2023-10-16 10:44:12 -0500

    why no node

---
+++ owenpodrx/src/components/brand/inputs.js
@@ -0,0 +1,7 @@
+import google from '../../assets/google.npg'
+import slack from '../../assets/slack.npg'
+import atlassian from '../../assets/atlassian.npg'
+import dropbox from '../../assets/dropbox.npg'
+import shopify from '../../assets/shopify.npg'
+
+export { google, slack, atlassian, dropbox, shopify}
