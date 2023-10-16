2e9d14bcc319f1f13d359acfc09239fd30360014
Owen <podziemskio@msoe.edu>
2023-10-15 19:45:43 -0500

    mania

---
+++ owenpodrx/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+import './index.css';
+
+//https://www.youtube.com/watch?v=F627pKNUCVQ
+
+ReactDOM.render(<App />, document.getElementById('root'));
